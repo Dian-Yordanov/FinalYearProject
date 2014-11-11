068249ecff36e8015c587b70ac17af230fa0c829
Xelnect <dido.jordanov7@gmail.com>
2014-11-12 00:10:30 +0200

    Hello 3rd year...nice to meet you on 12.11.2014....

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,34 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+    SpriteBatch square1Batch;
+	Texture img;
+    Texture square1Img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+        square1Batch = new SpriteBatch();
+        square1Img = new Texture("Square1.png");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+        batch.draw(square1Img, 0, 0);
+		batch.end();
+
+
+	}
+}
