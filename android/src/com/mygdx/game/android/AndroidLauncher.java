7deab4c99bb89117668101ebbaad02c7e4730c48
Xelnect <dido.jordanov7@gmail.com>
2014-11-12 01:41:13 +0200

    working menu

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -3,6 +3,8 @@ package com.mygdx.game.android;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -20,5 +22,19 @@ public class AndroidLauncher extends AndroidApplication {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.game_menu, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.new_game:
+                Toast.makeText(getApplicationContext(), "Sample Text1", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.help:
+                Toast.makeText(getApplicationContext(), "Sample Text2", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
