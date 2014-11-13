9e1f066f2dd5f50a68547e4191f4c5403c45e4a5
Dian Yordanov <dido.jordanov7@gmail.com>
2014-11-13 04:40:01 +0000

    dfdsfsdf

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -1,13 +1,16 @@
 package com.mygdx.game.android;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.files.FileHandle;
 import com.mygdx.game.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
@@ -28,7 +31,13 @@ public class AndroidLauncher extends AndroidApplication {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.option1:
-                Toast.makeText(getApplicationContext(), "Sample Text1", Toast.LENGTH_LONG).show();
+                //.makeText(getApplicationContext(), "Sample Text1", Toast.LENGTH_LONG).show();
+                Log.v("taggg", "ghg" );
+                FileHandle[] files = Gdx.files.internal("data/graphics/").list();
+                for(FileHandle file: files) {
+                    Toast.makeText(getApplicationContext(), file.name() + files.toString(), Toast.LENGTH_LONG).show();
+                    Log.v("taggg", file.name() + files.toString() );
+                }
                 return true;
             case R.id.option2:
                 Toast.makeText(getApplicationContext(), "Sample Text2", Toast.LENGTH_LONG).show();
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,17 +6,6 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-//import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
-
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	//Texture img;
