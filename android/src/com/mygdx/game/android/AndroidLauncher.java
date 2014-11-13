26df5ddb07c23e03336569b4675665ed42b78f59
Xelnect <dido.jordanov7@gmail.com>
2014-11-13 20:44:42 +0200

    external test
    String path =
    Environment.getExternalStorageDirectory().toString()+"/Android";
    Log.d("Files", "Path: " + path);
    File ff = new File(path);
    File file[] = ff.listFiles();
    Log.d("Files", "Size: "+ file.length);
    for (int i=0; i < file.length; i++)
    {
    Log.d("Files", "FileName:" + file[i].getName());
    }

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -1,6 +1,9 @@
 package com.mygdx.game.android;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,6 +16,7 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.badlogic.gdx.files.FileHandle;
 import com.mygdx.game.MyGdxGame;
 
+import java.io.File;
 import java.io.IOException;
 
 public class AndroidLauncher extends AndroidApplication {
@@ -33,49 +37,46 @@ public class AndroidLauncher extends AndroidApplication {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.option1:
-                //.makeText(getApplicationContext(), "Sample Text1", Toast.LENGTH_LONG).show();
-               // Log.v("taggg", "ghg" );
-                //Gdx.app.log("AssetPath", Gdx.files.internal("Square1.png").file().getAbsolutePath());
-
-                //FileHandle dirHandle = Gdx.files.absolute("/storage/emulated/0/");
-                //for (FileHandle entry: dirHandle.list()) {
-                //    Log.v("taggg", "1 " +  entry.name() + files.toString() );
-                //}
-
-                Gdx.app.log("AssetPath", Gdx.files.internal("/android/assets/Square1.png").file().getParent());
-
-                Gdx.app.log("AssetPath", Gdx.files.internal("/android/assets/Square1.png").file().getPath());
-                Gdx.app.log("taggg", "!" + Gdx.files.internal("/android/assets/").file().listFiles());
-                Gdx.app.log("taggg", "!" + Gdx.files.internal("/android/assets").file().listFiles());
 
 
-                Log.v("taggg", "!" + Gdx.files.internal("/").isDirectory());
-                Log.v("taggg", "!" + Gdx.files.internal("/").list().length);
+                ContextWrapper c = new ContextWrapper(this);
+                Log.v(c.getFilesDir().getPath(), "!" + Gdx.files.local(c.getFilesDir().getPath()).isDirectory());
+                Log.v(c.getApplicationInfo().dataDir , "!" + Gdx.files.local(c.getApplicationInfo().dataDir ).list().length);
+                Log.v("taggg", "!" + Gdx.files.getLocalStoragePath());
+                Log.v("taggg", "!" + Gdx.files.getExternalStoragePath());
 
-
-                Log.v("AssetPath", "!" + Gdx.files.internal("data/graphics/").file().isDirectory());
-                Log.v("taggg", "!" + Gdx.files.internal("/storage/emulated/0").file().list());
-
-                Log.v("taggg", "!" + Gdx.files.external("/Android/").list().length);
-                Log.v("taggg", "!" + Gdx.files.internal("/storage/emulated/0").isDirectory());
-                Log.v("taggg", "!" + Gdx.files.internal("/storage/emulated/0/Android").list().length);
-                Log.v("taggg", "!" + Gdx.files.absolute("/assets").list().length);
-
-
-                FileHandle[] files = Gdx.files.internal("/").list();
-                for(FileHandle file: files) {
-                    Log.v("taggg", "2 " +  file.name() );
+                File f = new File(c.getFilesDir().getPath());
+                File[] files11 = f.listFiles();
+                for (File inFile : files11) {
+                    if (inFile.isDirectory()) {
+                        Log.v("dfsdf","dsfsd");
+                    }
                 }
+
+                String path = Environment.getExternalStorageDirectory().toString()+"/Android";
+                Log.d("Files", "Path: " + path);
+                File ff = new File(path);
+                File file[] = ff.listFiles();
+                Log.d("Files", "Size: "+ file.length);
+                for (int i=0; i < file.length; i++)
+                {
+                    Log.d("Files", "FileName:" + file[i].getName());
+                }
+
+
+
                 FileHandle[] files1 = Gdx.files.internal("/").list();
                 for(FileHandle file2: files1) {
                     if(file2.name().equals("assets")){
-                        
                         Log.v("taggg", "4 " +  file2.name() );
+                        Log.v(file2.name(), "4 " +  Gdx.files.internal(file2.path()).isDirectory());
+                        FileHandle[] files3 = Gdx.files.internal(file2.name()).list();
+                        for(FileHandle file4: files3) {
+                            Log.v("taggg", "5 " +  file4.name() );
+                        }
                     }
-
                 }
 
-                Log.v("taggg", "3 " + files);
                 return true;
             case R.id.option2:
                 Toast.makeText(getApplicationContext(), "Sample Text2", Toast.LENGTH_LONG).show();
