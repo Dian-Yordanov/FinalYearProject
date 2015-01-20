fb77c6f15b5c8bb25f97caffdfae2a96d095eba5
Dian Yordanov <dido.jordanov7@gmail.com>
2015-01-20 00:20:00 +0000

    created the texture chooser
    created the texture chooser

--- android/src/com/activities/activity1.java
+++ android/src/com/activities/activity1.java
@@ -1,19 +1,96 @@
 package com.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.mygdx.game.android.R;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * Created by XelnectPC on 11/15/2014.
  */
-public class activity1 extends Activity {
+public class activity1 extends Activity{
+    private static final int SELECT_PICTURE = 1;
+    private String  selectedImagePath;
+    ImageView image;
+    Button button;
+    public static Bitmap bMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity1);
+        image = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button);
 
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent, SELECT_PICTURE);
+                image.setImageBitmap(bMap);
+            }
+
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE)
+            {
+                Uri selectedImageUri = data.getData();
+                selectedImagePath = getPath(selectedImageUri);
+                try {
+                    FileInputStream fileis=new FileInputStream(selectedImagePath);
+                    BufferedInputStream bufferedstream=new BufferedInputStream(fileis);
+                    byte[] bMapArray= new byte[bufferedstream.available()];
+                    bufferedstream.read(bMapArray);
+                    bMap = BitmapFactory.decodeByteArray(bMapArray, 0, bMapArray.length);
+                    //Here you can set this /Bitmap image to the button background image
+
+                    if (fileis != null)
+                    {
+                        fileis.close();
+                    }
+                    if (bufferedstream != null)
+                    {
+                        bufferedstream.close();
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
     }
 }
