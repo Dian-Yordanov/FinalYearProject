c57dd185217fe44e53870b45cbd910e830f75d20
Dian Yordanov <dido.jordanov7@gmail.com>
2015-02-05 03:27:58 +0000

    starting activity made efficient

--- android/src/com/activities/StartingActivity.java
+++ android/src/com/activities/StartingActivity.java
@@ -15,67 +15,65 @@ import com.mygdx.game.android.R;
 import com.mygdx.game.android.TriangullarTillingLauncher;
 
 public class StartingActivity extends Activity {
-    //public static Button SquareTillingLauncherButton;
+    public static Button SquareTillingLauncherButton;
     public static Button HexagonalTillingLauncherButton;
     public static Button TriangullarTillingLauncherButton;
-
-    private Button SquareTillingLauncherButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.starting_activity);
-        //SquareTillingLauncherButton = (Button) findViewById(R.id.SquareTillingLauncher);
-        HexagonalTillingLauncherButton = (Button) findViewById(R.id.HexagonalTillingLauncher);
-        TriangullarTillingLauncherButton = (Button) findViewById(R.id.TriangullarTillingLauncher);
 
-        callClassSquareTillingLauncher(SquareTillingLauncherButton1, R.id.squareTillingLauncher,"");
+        callClassSquareTillingLauncher(SquareTillingLauncherButton, R.id.squareTillingLauncher
+                ,"SquareTillingLauncher", "com.mygdx.game.android.SquareTillingLauncher"
+                ,"data/ii_square_tilling.png");
+        callClassSquareTillingLauncher(HexagonalTillingLauncherButton, R.id.hexagonalTillingLauncher
+                ,"HexagonalTillingLauncher", "com.mygdx.game.android.HexagonalTillingLauncher"
+                ,"data/ii_hexagonal_tilling.png");
+        callClassSquareTillingLauncher(TriangullarTillingLauncherButton, R.id.triangullarTillingLauncher
+                ,"TriangullarTillingLauncher", "com.mygdx.game.android.TriangullarTillingLauncher"
+                ,"data/ii_triangular_tilling.png","data/ii_triangular_tilling_2.png");
 
-        callTheClickListeners();
     }
 
-    public void callTheClickListeners(){
-        /*
-        SquareTillingLauncherButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                LogicalClassForRenderCallingActivites.setupPatternStyle("SquareTillingLauncher");
-                MyGdxGame.imageNameToBeSavedMGG="data/ii_square_tilling.png";
-                Intent i = new Intent(StartingActivity.this,SquareTillingLauncher.class);
-                startActivity(i);
-            }
-        });
-        */
-        HexagonalTillingLauncherButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                LogicalClassForRenderCallingActivites.setupPatternStyle("HexagonalTillingLauncher");
-                MyGdxGame.imageNameToBeSavedMGG="data/ii_hexagonal_tilling.png";
-                Intent i = new Intent(StartingActivity.this,HexagonalTillingLauncher.class);
-                startActivity(i);
-            }
-        });
-        TriangullarTillingLauncherButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                LogicalClassForRenderCallingActivites.setupPatternStyle("TriangullarTillingLauncher");
-                MyGdxGame.imageNameToBeSavedMGG="data/ii_triangular_tilling.png";
-                MyGdxGame.imageNameToBeSavedMGG2="data/ii_triangular_tilling_2.png";
-                Intent i = new Intent(StartingActivity.this,TriangullarTillingLauncher.class);
-                startActivity(i);
-            }
-        });
-    }
-    public void callClassSquareTillingLauncher(Button buttonTobeUsed, int RidObjectToBeCalled, String image1Name ){
+    public void callClassSquareTillingLauncher(Button buttonTobeUsed, int RidObjectToBeCalled,
+                                                final String nameOfThisLauncher, final String nameOfclassToLaunch
+            ,final String image1Name ){
         //TriangullarTillingLauncher = (Button) findViewById(R.id.TriangullarTillingLauncher);
         buttonTobeUsed = (Button) findViewById(RidObjectToBeCalled);
         buttonTobeUsed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                LogicalClassForRenderCallingActivites.setupPatternStyle("SquareTillingLauncher");
-                MyGdxGame.imageNameToBeSavedMGG="data/ii_square_tilling.png";
-                Intent i = new Intent(StartingActivity.this,SquareTillingLauncher.class);
-                startActivity(i);
+                LogicalClassForRenderCallingActivites.setupPatternStyle(nameOfThisLauncher);
+                MyGdxGame.imageNameToBeSavedMGG=image1Name;
+                try {
+                    Class<?> act = Class.forName(nameOfclassToLaunch);
+                    Intent i = new Intent(StartingActivity.this,act);
+                    startActivity(i);
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    public void callClassSquareTillingLauncher(Button buttonTobeUsed, int RidObjectToBeCalled,
+                                               final String nameOfThisLauncher, final String nameOfclassToLaunch
+            ,final String image1Name,final String image2Name ){
+        //TriangullarTillingLauncher = (Button) findViewById(R.id.TriangullarTillingLauncher);
+        buttonTobeUsed = (Button) findViewById(RidObjectToBeCalled);
+        buttonTobeUsed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                LogicalClassForRenderCallingActivites.setupPatternStyle(nameOfThisLauncher);
+                MyGdxGame.imageNameToBeSavedMGG=image1Name;
+                MyGdxGame.imageNameToBeSavedMGG2=image2Name;
+                try {
+                    Class<?> act = Class.forName(nameOfclassToLaunch);
+                    Intent i = new Intent(StartingActivity.this,act);
+                    startActivity(i);
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
