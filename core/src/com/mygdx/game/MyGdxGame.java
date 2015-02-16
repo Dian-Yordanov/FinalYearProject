98fa1842710df7bc9baa605f6314fdeabf9f2622
Dian Yordanov <dido.jordanov7@gmail.com>
2015-02-16 22:39:32 +0000

    idea done but it is still not working

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -284,7 +284,7 @@ public class MyGdxGame extends ApplicationAdapter {
                     sp0lastX = sp0.getX() *2/3;
                     //positionSpritesForDrawing(i, ii, 0, 0);
                 }
-                if (i % 2 != 0) {
+                if (i % 2 != 0  && ii %2!=0) {
                     positionSpritesForDrawing(i,ii,0,sp2.getHeight());
                 }
                // if (ii % 2 == 0) {
