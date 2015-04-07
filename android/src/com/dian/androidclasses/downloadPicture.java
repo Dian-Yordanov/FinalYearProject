abba621f119653aef03ab51120df298723114a5b
Xynnect <dido.jordanov7@gmail.com>
2015-04-07 05:56:47 +0100

    added the download picture class

---
+++ android/src/com/dian/androidclasses/downloadPicture.java
@@ -0,0 +1,29 @@
+package com.dian.androidclasses;
+
+import java.awt.*;
+import java.io.*;
+import java.net.URL;
+
+/**
+ * Created by zyan on 06/04/15.
+ */
+public class downloadPicture {
+    public downloadPicture() throws IOException {
+        URL url = new URL("http://upload.wikimedia.org/wikipedia/commons/c/cc/Rectangle_.png");
+        InputStream in = new BufferedInputStream(url.openStream());
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] buf = new byte[1024];
+        int n = 0;
+        while (-1!=(n=in.read(buf)))
+        {
+            out.write(buf, 0, n);
+        }
+        out.close();
+        in.close();
+        byte[] response = out.toByteArray();
+        FileOutputStream fos = new FileOutputStream("/home/zyan/IdeaProjects/Rectangle_.png");
+        fos.write(response);
+        fos.close();
+        //commit
+    }
+}
